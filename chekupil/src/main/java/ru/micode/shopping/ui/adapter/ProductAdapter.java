67372b56c82a5d678e6c90d7f1d819959a948270
alexander.smirnov <alfie.work@gmail.com>
2019-06-08 21:53:39 +0300

    [Smart  Planet R] Enter product entity

---
+++ chekupil/src/main/java/ru/micode/shopping/ui/adapter/ProductAdapter.java
@@ -0,0 +1,116 @@
+package ru.micode.shopping.ui.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import ru.micode.shopping.BuildConfig;
+import ru.micode.shopping.R;
+import ru.micode.shopping.model.ex.ExBuy;
+import ru.smart.planet.web.Product;
+import ru.micode.shopping.ui.adapter.recycler.ItemTouchListener;
+import ru.micode.shopping.ui.adapter.recycler.MyRecyclerAdapter;
+import ru.micode.shopping.ui.adapter.recycler.MyRecyclerHolder;
+
+public class ProductAdapter extends MyRecyclerAdapter<Product> {
+
+    public ProductAdapter(Context context, ItemTouchListener<Product> actionListener) {
+        super(context, new Product(), new ArrayList<Product>(), actionListener);
+    }
+
+    @Override
+    public MyRecyclerHolder<Product> onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ProductHolder(context, parent, this);
+    }
+
+    @Override
+    public void onBindViewHolder(MyRecyclerHolder<Product> holder, int position) {
+        holder.bind(values.get(position), position);
+    }
+
+    public Product getByPosition(int pos) {
+        if (pos >= 0 && pos < values.size()) {
+            return values.get(pos);
+        }
+        return null;
+    }
+
+
+    public static class ProductHolder extends MyRecyclerHolder<Product> {
+
+        private TextView title;
+        private TextView description;
+        private ImageView image;
+        private ProgressBar loadingBar;
+
+        public ProductHolder(Context context, ViewGroup group, ActionListener<Product> actionListener) {
+            super(context, R.layout.recipe_adapter, group, actionListener);
+            title = itemView.findViewById(R.id.product_view_title);
+            //favoriteImage = itemView.findViewById(R.id.product_favorite); //in productAdapter
+            description = itemView.findViewById(R.id.product_view_description);
+            image = itemView.findViewById(R.id.product_view_image);
+            loadingBar = itemView.findViewById(R.id.product_view_loading);
+        }
+//
+//        /**
+//         * Установка статуса избранного рецепта
+//         *
+//         * @param isFavorite
+//         */
+//        private void setFavoriteState(boolean isFavorite) {
+//            if (isFavorite) {
+//                favoriteImage.setImageResource(R.drawable.ic_star);
+//            } else {
+//                favoriteImage.setImageResource(R.drawable.ic_star_border);
+//            }
+//        }
+
+        @Override
+        protected void bindData(final Product value, final int position) {
+            title.setText(value.getTitle());
+            description.setText(StringUtils.abbreviate(value.getDescription(), 150));
+//            favoriteImage.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    boolean isFav = actionListener.itemAction(Action.MODIFIED, position, view, value);
+//                    setFavoriteState(isFav);
+//                }
+//            });
+            boolean b = actionListener.itemAction(Action.BIND, position, null, value);
+//            setFavoriteState(b);
+//            if (!value.getImages().isEmpty()) {
+//                loadingBar.setVisibility(View.VISIBLE);
+//                String srcImage = value.getImages().get(0);
+//                // Загрузить картинку
+//                Picasso picasso = Picasso.get();
+//                picasso.setIndicatorsEnabled(BuildConfig.DEBUG);
+//                picasso.load(srcImage)
+//                    .into(image, new Callback() {
+//                        @Override
+//                        public void onSuccess() {
+//                            loadingBar.setVisibility(View.GONE);
+//                        }
+//
+//                        @Override
+//                        public void onError(Exception e) {
+//                            loadingBar.setVisibility(View.GONE);
+//                        }
+//                    });
+//            } else {
+//                loadingBar.setVisibility(View.GONE);
+//            }
+        }
+    }
+}
